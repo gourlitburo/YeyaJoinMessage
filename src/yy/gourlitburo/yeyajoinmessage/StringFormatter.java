f2e5259a7cc0f7a05fc8f3619610796d104396a0
Zack Guard <zacharyguard@gmail.com>
2019-06-30 16:35:34 +0800

    Various interpolation improvements
    Change interpolation pattern to #{}, keep compiled regex patterns, check if key exists before attempting to interpolate it, include non-color format codes.

--- src/yy/gourlitburo/yeyajoinmessage/StringFormatter.java
+++ src/yy/gourlitburo/yeyajoinmessage/StringFormatter.java
@@ -6,23 +6,27 @@ import java.util.regex.Pattern;
 
 public class StringFormatter {
 
-  private static final String PATTERN_STR = "\\{[A-Z]+\\}";
+  private static final String PATTERN_INTERP = "#\\{[A-Z]+\\}";
+  private static final Pattern patternInterp = Pattern.compile(PATTERN_INTERP);
+  private static final String PATTERN_COLOR = "&([0-9a-flmnor])";
+  private static final Pattern patternColor = Pattern.compile(PATTERN_COLOR);
 
   public String colorize(String message) {
-    return message.replaceAll("&([0-9a-fr])", "\u00A7$1");
+    return patternColor.matcher(message).replaceAll("\u00A7$1");
   }
 
   public String interpolate(String message, Map<String, String> values) {
-    Pattern pattern = Pattern.compile(PATTERN_STR);
-    Matcher matcher = pattern.matcher(message);
+    Matcher matcher = patternInterp.matcher(message);
     StringBuilder builder = new StringBuilder();
     int prevEnd = 0;
     while (matcher.find()) {
       String matched = matcher.group(0);
-      String key = matched.substring(1, matched.length() - 1);
-      builder.append(message.substring(prevEnd, matcher.start()));
-      builder.append(values.get(key));
-      prevEnd = matcher.end();
+      String key = matched.substring(2, matched.length() - 1);
+      if (values.containsKey(key)) {
+        builder.append(message.substring(prevEnd, matcher.start()));
+        builder.append(values.get(key));
+        prevEnd = matcher.end();
+      }
     }
     if (prevEnd != message.length()) {
       builder.append(message.substring(prevEnd));
